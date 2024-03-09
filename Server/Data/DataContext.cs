3306725f348ee51d86128eac05ff8d6a1ea3d200
Maor Solo <MaorSolo95@Gmail.com>
2024-03-09 17:29:33 +0100

    Add project files.

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+           : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=TestDB_TimeLogger.db");
+        }
+    }
+}
